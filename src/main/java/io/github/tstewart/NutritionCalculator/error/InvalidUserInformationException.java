fb623a29825d7b3603772f2b4d5254940dd7b15d
T. Stewart <tomstewart20@gmail.com>
2020-01-07 11:27:09 +0000

    Added invalid user error and comments

---
+++ src/main/java/io/github/tstewart/NutritionCalculator/error/InvalidUserInformationException.java
@@ -0,0 +1,7 @@
+package io.github.tstewart.NutritionCalculator.error;
+
+/**
+ * Thrown if the user info class contains information that cannot be calculated (e.g. wrong age)
+ */
+public class InvalidUserInformationException extends Exception {
+}
--- src/main/java/io/github/tstewart/NutritionCalculator/strategies/NutritionCalculationStrategy.java
+++ src/main/java/io/github/tstewart/NutritionCalculator/strategies/NutritionCalculationStrategy.java
@@ -5,6 +5,11 @@ import io.github.tstewart.NutritionCalculator.UserNutrition;
 
 public class NutritionCalculationStrategy implements UserNutrition.UserNutritionCalculation {
 
+    /**
+     * Calculates nutritional information (calories, nutrients required) with the provided user information
+     * @param info User information (age, gender, etc.)
+     * @return Nutrition required for this user
+     */
     @Override
     public UserNutrition calculateNutritionalInformation(UserInfo info) {
         return null;
