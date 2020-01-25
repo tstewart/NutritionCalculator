27dc213f34d6bb81cc4f47ee79e9a8e5a9a5f061
T. Stewart <tomstewart20@gmail.com>
2020-01-25 16:38:36 +0000

    Added nutrition calculation strategy closes #3

--- src/main/java/io/github/tstewart/NutritionCalculator/strategies/NutritionCalculationStrategy.java
+++ src/main/java/io/github/tstewart/NutritionCalculator/strategies/NutritionCalculationStrategy.java
@@ -7,12 +7,49 @@ public class NutritionCalculationStrategy implements UserNutrition.UserNutrition
 
     /**
      * Calculates nutritional information (calories, nutrients required) with the provided user information
+     * Calories are calculated with the following formula:
+     *
+     * Adult male: 66 + (6.3 x body weight in lbs.) + (12.9 x height in inches) - (6.8 x age in years)
+     * Adult female: 655 + (4.3 x weight in lbs.) + (4.7 x height in inches) - (4.7 x age in years)
+     *
+     * These values are then multiplied by 1.2, assuming a normal amount of exercise by the user.
+     *
      * @param info User information (age, gender, etc.)
      * @return Nutrition required for this user
      */
     @Override
     public UserNutrition calculateNutritionalInformation(UserInfo info) {
-        return null;
+        UserNutrition nutrition = new UserNutrition();
+        Double calorieCalculation;
+
+        if(info.getGender() == UserInfo.Gender.MALE) {
+            calorieCalculation = (66 + (convertKgToLb(info.getWeight()) * 6.3) + (convertCmToIn(info.getHeight()) * 12.9) - (info.getAge() * 6.8));
+        }
+        else {
+            calorieCalculation = (655 + (convertKgToLb(info.getWeight()) * 4.3) + (convertCmToIn(info.getHeight()) * 4.7) - (info.getAge() * 4.7));
+        }
+        calorieCalculation *= 1.2;
+        nutrition.setCaloriesRequired((int) Math.floor(calorieCalculation));
+        return nutrition;
+    }
+
+    private double convertKgToLb(double weightKg) {
+        return weightKg * ImperialConversion.KgToLb.conversionRate;
+    }
+
+    private double convertCmToIn(double heightCm) {
+        return heightCm * ImperialConversion.cmToIn.conversionRate;
+    }
+}
+
+enum ImperialConversion {
+    KgToLb(2.20462),
+    cmToIn(0.393701);
+
+    double conversionRate;
+
+    ImperialConversion(double conversionRate) {
+        this.conversionRate = conversionRate;
     }
 
 }
