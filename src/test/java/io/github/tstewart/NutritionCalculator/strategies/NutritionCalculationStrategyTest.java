20cb9a1f560e555a85e071fa29079ea83dbbf84b
T. Stewart <tomstewart20@gmail.com>
2020-02-26 12:06:59 +0000

    Added testing closes #5

--- src/test/java/io/github/tstewart/NutritionCalculator/strategies/NutritionCalculationStrategyTest.java
+++ src/test/java/io/github/tstewart/NutritionCalculator/strategies/NutritionCalculationStrategyTest.java
@@ -22,11 +22,33 @@ public class NutritionCalculationStrategyTest {
 
         //assert
         // Expected results from the calculation strategy
-        Assert.assertEquals(1830, userNutrition.getCaloriesRequired(), 0);
+        Assert.assertEquals(2363, userNutrition.getCaloriesRequired(), 0);
         Assert.assertEquals(330, userNutrition.getCarbohydratesRequired(), 0);
-        Assert.assertEquals(40, userNutrition.getFatRequired(), 0);
-        Assert.assertEquals(112, userNutrition.getProteinRequired(), 0);
-        Assert.assertEquals(27, userNutrition.getFiberRequired(), 0);
+        Assert.assertEquals(52, userNutrition.getFatRequired(), 0);
+        Assert.assertEquals(45, userNutrition.getProteinRequired(), 0);
+        Assert.assertEquals(35, userNutrition.getFiberRequired(), 0);
+    }
+
+    @Test
+    public void calculatesFemaleNutrition() {
+        //arrange
+        final UserInfo userInfo = new UserInfo();
+        final UserNutrition.UserNutritionCalculation strategy = new NutritionCalculationStrategy();
+        userInfo.setAge(30);
+        userInfo.setGender(UserInfo.Gender.FEMALE);
+        userInfo.setHeight(180);
+        userInfo.setWeight(75);
+
+        //act
+        final UserNutrition userNutrition = strategy.calculateNutritionalInformation(userInfo);
+
+        //assert
+        // Expected results from the calculation strategy
+        Assert.assertEquals(2414, userNutrition.getCaloriesRequired(), 0);
+        Assert.assertEquals(413, userNutrition.getCarbohydratesRequired(), 0);
+        Assert.assertEquals(53, userNutrition.getFatRequired(), 0);
+        Assert.assertEquals(56.25, userNutrition.getProteinRequired(), 0);
+        Assert.assertEquals(36, userNutrition.getFiberRequired(), 0);
     }
 
 }
